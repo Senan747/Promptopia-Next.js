ebf28b4e5271cd418d2fbd64bbfd5cc9ed046e05
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-09-13 08:54:57 +0400

    database

---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnnected = false;
+
+export const connetToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnnected) {
+    console.log("mongo db is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "sharePrompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnnected = true;
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
